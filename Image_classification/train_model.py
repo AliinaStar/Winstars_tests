372b5e2f1e3d613801a2bbc578cbd85934e765cc
AliinaStar <128737579+AliinaStar@users.noreply.github.com>
2025-03-03 08:27:00 +0200

    Added dockstrings to the functions

--- Image_classification/train_model.py
+++ Image_classification/train_model.py
@@ -6,6 +6,15 @@ from abc import ABC, abstractmethod
 from sklearn.ensemble import RandomForestClassifier as SklearnRF
 
 def load_data():
+    '''
+    Laod MNIST dataset and separate to train and test data
+
+    Returns:
+        X_train (np.array): Training images
+        y_train (np.array): Training labels
+        X_test (np.array): Test images
+        y_test (np.array): Test labels
+    '''
     training_transforms = transforms.Compose([
         transforms.RandomResizedCrop(28),
         transforms.RandomRotation(45),
@@ -35,34 +44,67 @@ def load_data():
 
 
 class MnistClassifierInterface(ABC):
-    """
-    Represents an interface class for MNIST classifiers.
-    """
+    '''
+    Interface for MNIST classifiers
+    '''
     @abstractmethod
     def train_model(self, X_train, y_train):
-        """Навчання моделі на тренувальних даних"""
+        '''
+        Train the model on the training data
+
+        Args:
+            X_train (np.array): Training images
+            y_train (np.array): Training labels
+        '''
         pass
     
     @abstractmethod
     def predict_model(self, X_test):
-        """Прогнозування на нових даних"""
+        '''
+        Predict the labels for the test data
+
+        Args:
+            X_test (np.array): Test images
+        '''
         pass
 
 
 class RandomForestClassifier(MnistClassifierInterface):
     """
     Random Forest classifier for MNIST
+
+    Attributes:
+        model (SklearnRF): Random Forest model
     """
     def __init__(self, model=None):
+        '''
+        Initialize the Random Forest classifier
+
+        Args:
+            model (SklearnRF): Random Forest model
+        '''
         self.model = model
         
     def train_model(self, X_train, y_train):
+        '''
+        Train the Random Forest model
+
+        Args:
+            X_train (np.array): Training images
+            y_train (np.array): Training labels
+        '''
         X_train = X_train.reshape(-1, 28*28)
         self.model = SklearnRF()
         self.model.fit(X_train, y_train)
         return self.model
 
     def predict_model(self, X_test):
+        '''
+        Predict the labels for the test data
+
+        Args:
+            X_test (np.array): Test images
+        '''
         X_test = X_test.reshape(-1, 28*28)
         return self.model.predict(X_test)
 
@@ -70,8 +112,24 @@ class RandomForestClassifier(MnistClassifierInterface):
 class FeedforwardNeuralNetworkClassifier(nn.Module, MnistClassifierInterface):
     """
     Neural Network classifier for MNIST
+
+    Attributes:
+        fc1 (nn.Linear): First fully connected layer
+        fc2 (nn.Linear): Second fully connected layer
+        fc3 (nn.Linear): Third fully connected layer
+        activation (nn.ReLU): Activation function
+        loss_function (nn.CrossEntropyLoss): Loss function
+        optimizer (optim.Adam): Optimizer
     """
     def __init__(self, input_size=784, hidden_sizes=[256, 128], output_size=10):
+        '''
+        Initialize the Feedforward Neural Network classifier
+
+        Args:
+            input_size (int): Input size
+            hidden_sizes (list): List of hidden layer sizes
+            output_size (int): Output size
+        '''
         super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_sizes[0])
         self.fc2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
@@ -81,6 +139,12 @@ class FeedforwardNeuralNetworkClassifier(nn.Module, MnistClassifierInterface):
         self.optimizer = optim.Adam(self.parameters(), lr=0.001)
         
     def forward(self, x):
+        '''
+        Forward pass of the neural network
+
+        Args:
+            x (torch.Tensor): Input tensor
+        '''
         x = x.view(-1, 784)
         x = self.activation(self.fc1(x))
         x = self.activation(self.fc2(x))
@@ -88,7 +152,15 @@ class FeedforwardNeuralNetworkClassifier(nn.Module, MnistClassifierInterface):
         return x
 
     def train_model(self, X_train, y_train, epochs=5, batch_size=64):
-        """ Навчання моделі (перетворює numpy → Tensor) """
+        '''
+        Train the Feedforward Neural Network model
+
+        Args:
+            X_train (np.array): Training images
+            y_train (np.array): Training labels
+            epochs (int): Number of epochs
+            batch_size (int): Batch size
+        '''
         X_train_tensor = torch.tensor(X_train, dtype=torch.float32).view(-1, 1, 28, 28) / 255.0
         y_train_tensor = torch.tensor(y_train, dtype=torch.long)
         dataset = torch.utils.data.TensorDataset(X_train_tensor, y_train_tensor)
@@ -108,7 +180,12 @@ class FeedforwardNeuralNetworkClassifier(nn.Module, MnistClassifierInterface):
         return self
 
     def predict_model(self, X_test):
-        """ Передбачення на тестових даних (numpy → Tensor) """
+        '''
+        Predict the labels for the test data
+
+        Args:
+            X_test (np.array): Test images
+        '''
         X_test_tensor = torch.tensor(X_test, dtype=torch.float32).view(-1, 1, 28, 28) / 255.0
         with torch.no_grad():
             outputs = self(X_test_tensor)
@@ -120,8 +197,16 @@ class FeedforwardNeuralNetworkClassifier(nn.Module, MnistClassifierInterface):
 class CnnClassifier(nn.Module, MnistClassifierInterface):
     """
     CNN classifier for MNIST using PyTorch
+
+    Attributes:
+        model (nn.Sequential): CNN model
+        loss_function (nn.CrossEntropyLoss): Loss function
+        optimizer (optim.Adam): Optimizer
     """
     def __init__(self):
+        '''
+        Initialize the CNN classifier
+        '''
         super(CnnClassifier, self).__init__()
         self.model = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3),  # Convolutional layer
@@ -140,7 +225,15 @@ class CnnClassifier(nn.Module, MnistClassifierInterface):
         self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
 
     def train_model(self, X_train, y_train, epochs=5, batch_size=64):
-        """Training the CNN model"""
+        '''
+        Train the CNN model
+
+        Args:
+            X_train (np.array): Training images
+            y_train (np.array): Training labels
+            epochs (int): Number of epochs
+            batch_size (int): Batch size
+        '''
         X_train_tensor = torch.tensor(X_train, dtype=torch.float32).view(-1, 1, 28, 28) / 255.0
         y_train_tensor = torch.tensor(y_train, dtype=torch.long)
         dataset = torch.utils.data.TensorDataset(X_train_tensor, y_train_tensor)
@@ -159,7 +252,12 @@ class CnnClassifier(nn.Module, MnistClassifierInterface):
             print(f"Epoch [{epoch+1}/{epochs}], Loss: {total_loss/len(train_loader):.4f}")
 
     def predict_model(self, X_test):
-        """Predicting with the CNN model"""
+        '''
+        Predict the labels for the test data
+
+        Args:
+            X_test (np.array): Test images
+        '''
         X_test_tensor = torch.tensor(X_test, dtype=torch.float32).view(-1, 1, 28, 28) / 255.0
         with torch.no_grad():
             outputs = self.model(X_test_tensor)
@@ -169,16 +267,19 @@ class CnnClassifier(nn.Module, MnistClassifierInterface):
 
 
 class MnistClassifier:
-    """
-    Mnist classifier based on the selected algorithm
-    """
+    '''
+    Class to initialize the classifier based on the selected algorithm
+
+    Attributes:
+        classifier (MnistClassifierInterface): Classifier based on the selected algorithm
+    '''
     def __init__(self, algorithm):
-        """
-        Ініціалізує відповідний класифікатор на основі вибраного алгоритму
-        
-        Parameters:
-        algorithm (str): Тип алгоритму ('cnn', 'rf', або 'nn')
-        """
+        '''
+        Initialize the classifier based on the selected algorithm
+
+        Args:
+            algorithm (str): Algorithm to use for classification
+        '''
         if algorithm == 'rf':
             self.classifier = RandomForestClassifier()
         elif algorithm == 'nn':
@@ -186,4 +287,4 @@ class MnistClassifier:
         elif algorithm == 'cnn':
             self.classifier = CnnClassifier()
         else:
-            raise ValueError(f"Невідомий алгоритм: {algorithm}. Використовуйте 'cnn', 'rf', або 'nn'")
+            raise ValueError(f"Wrong algorithm: {algorithm}. Use 'cnn', 'rf', or 'nn'")
